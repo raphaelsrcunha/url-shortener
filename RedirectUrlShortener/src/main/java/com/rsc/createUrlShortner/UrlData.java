5c2ccd40ec39bc3a2efb0bddf8d9a347a939488d
Raphael Cunha <raphaelsrcunha@gmail.com>
2024-11-21 16:59:41 -0500

    first commit

---
+++ RedirectUrlShortener/src/main/java/com/rsc/createUrlShortner/UrlData.java
@@ -0,0 +1,16 @@
+package com.rsc.createUrlShortner;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
+
